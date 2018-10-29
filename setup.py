b5d4fc6025b14ba143d24f87410b1728d6f6d246
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-10-29 17:33:30 +0100

    Add license classifier

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
     ],
     keywords="simple climate model",
     license="GNU Affero General Public License v3.0 or later",
