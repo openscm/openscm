f9b9539cc71fa52c92d1b284cee26539bdcd1e76
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-02-01 11:29:35 +1100

    Split out scmdataframebase submodule to avoid circular imports

--- openscm/highlevel.py
+++ openscm/highlevel.py
@@ -3,15 +3,8 @@ The OpenSCM high-level API provides high-level functionality around
 single model runs.  This includes reading/writing input and output
 data, easy setting of parameters and stochastic ensemble runs.
 """
-from datetime import datetime
-from dateutil import parser
-
-
-from pyam import IamDataFrame
-
-
 from .core import Core
-
+from .scmdataframebase import ScmDataFrameBase
 
 class OpenSCM(Core):
     """
@@ -21,24 +14,6 @@ class OpenSCM(Core):
     """
 
     pass
-
-class ScmDataFrameBase(IamDataFrame):
-    """This base is the class other libraries can subclass
-
-    Having such a subclass avoids a potential circularity where e.g. openscm imports ScmDataFrame as well as Pymagicc, but Pymagicc wants to import ScmDataFrame and hence to try and import ScmDataFrame you have to import ScmDataFrame itself (hence the circularity).
-    """
-    def _format_datetime_col(self):
-        if isinstance(self.data["time"].iloc[0], str):
-            def convert_str_to_datetime(inp):
-                return parser.parse(inp)
-
-            self.data["time"] = self.data["time"].apply(convert_str_to_datetime)
-
-        not_datetime = [not isinstance(x, datetime) for x in self.data["time"]]
-        if any(not_datetime):
-            bad_values = self.data[not_datetime]["time"]
-            error_msg = "All time values must be convertible to datetime. The following values are not:\n{}".format(bad_values)
-            raise ValueError(error_msg)
 
 
 class ScmDataFrame(ScmDataFrameBase):
---
+++ openscm/scmdataframebase.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from dateutil import parser
+
+
+from pyam import IamDataFrame
+
+
+class ScmDataFrameBase(IamDataFrame):
+    """This base is the class other libraries can subclass
+
+    Having such a subclass avoids a potential circularity where e.g. openscm imports ScmDataFrame as well as Pymagicc, but Pymagicc wants to import ScmDataFrame and hence to try and import ScmDataFrame you have to import ScmDataFrame itself (hence the circularity).
+    """
+    def _format_datetime_col(self):
+        if isinstance(self.data["time"].iloc[0], str):
+            def convert_str_to_datetime(inp):
+                return parser.parse(inp)
+
+            self.data["time"] = self.data["time"].apply(convert_str_to_datetime)
+
+        not_datetime = [not isinstance(x, datetime) for x in self.data["time"]]
+        if any(not_datetime):
+            bad_values = self.data[not_datetime]["time"]
+            error_msg = "All time values must be convertible to datetime. The following values are not:\n{}".format(bad_values)
+            raise ValueError(error_msg)
