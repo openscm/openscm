883f89676738c1d5dfe994ecffc139e9cb28a133
Zeb Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-02-15 19:00:38 +0100

    Add AdapterTester class

---
+++ tests/adapters/__init__.py
@@ -0,0 +1,20 @@
+from abc import ABCMeta, abstractmethod
+import pytest
+
+
+@pytest.fixture(scope="function")
+def test_adapter(request):
+    try:
+        yield request.cls.tadapter()
+    except TypeError:
+        pytest.skip("{} cannot be instantiated".format(str(request.cls.tadapter)))
+
+
+class AdapterTester():
+    @property
+    @abstractmethod
+    def tadapter(self):
+        pass
+
+    def test_initialize(self, test_adapter):
+        test_adapter.initialize()
