50938aa450ca7ef08aa850b01fd9526169eb513b
Sven Willner <sven.willner@gmail.com>
2019-02-22 08:38:27 +0100

    Sort classifiers

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ CLASSIFIERS = [
     #   5 - Production/Stable
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
-    "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
 ]
 REQUIREMENTS_INSTALL = [
     "numpy",
