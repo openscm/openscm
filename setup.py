8874a3396e21d1ce48654ddb350f5fafbd1b8424
Sven Willner <sven.willner@gmail.com>
2019-02-22 08:33:13 +0100

    Update setup.py to use constants at top

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ OpenSCM
 -------
 
 A unifying interface for Simple Climate Models.
-
 """
 
 import versioneer
@@ -12,7 +11,35 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 
-REQUIREMENTS = [
+NAME = "openscm"
+PACKAGES = ["openscm"]
+SHORT_DESCRIPTION = "A unifying interface for Simple Climate Models"
+KEYWORDS = ["simple climate model"]
+AUTHORS = [
+    ("Robert Gieseke", "robert.gieseke@pik-potsdam.de"),
+    ("Jared Lewis", "jared.lewis@climate-energy-college.org"),
+    ("Zeb Nicholls", "zebedee.nicholls@climate-energy-college.org"),
+    ("Sven Willner", "sven.willner@pik-potsdam.de"),
+]
+URL = "https://github.com/openclimatedata/openscm"
+PROJECT_URLS = {
+    "Bug Reports": "https://github.com/openclimatedata/openscm/issues",
+    "Documentation": "https://openscm.readthedocs.io/en/latest",
+    "Source": "https://github.com/openclimatedata/openscm",
+}
+LICENSE = "GNU Affero General Public License v3.0 or later"
+CLASSIFIERS = [
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+]
+REQUIREMENTS_INSTALL = [
     "numpy",
     "pint",
     "pandas",
@@ -23,32 +50,35 @@ REQUIREMENTS_NOTEBOOKS = ["matplotlib", "notebook", "seaborn"]
 REQUIREMENTS_TESTS = ["codecov", "nbval", "pytest", "pytest-cov"]
 REQUIREMENTS_DOCS = ["sphinx>=1.4", "sphinx_rtd_theme", "sphinx-autodoc-typehints"]
 REQUIREMENTS_DEPLOY = ["setuptools>=38.6.0", "twine>=1.11.0", "wheel>=0.31.0"]
+REQUIREMENTS_DEV = (
+    ["flake8", "black"]
+    + REQUIREMENTS_NOTEBOOKS
+    + REQUIREMENTS_TESTS
+    + REQUIREMENTS_DOCS
+    + REQUIREMENTS_DEPLOY
+)
 
-REQUIREMENTS_EXTRAS = {
-    "notebooks": REQUIREMENTS_NOTEBOOKS,
-    "docs": REQUIREMENTS_DOCS,
-    "tests": REQUIREMENTS_TESTS,
-    "deploy": REQUIREMENTS_DEPLOY,
-    "dev": [
-        *["flake8", "black"],
-        *REQUIREMENTS_NOTEBOOKS,
-        *REQUIREMENTS_TESTS,
-        *REQUIREMENTS_DOCS,
-        *REQUIREMENTS_DEPLOY,
-    ],
-}
 
-REQUIREMENTS_MODELS = {}
 """
 When implementing an additional adapter, include your adapter NAME here as:
 ```
 "NAME": [ ... additional pip modules you need ... ],
 ```
 """
+REQUIREMENTS_MODELS = {}
+
+REQUIREMENTS_EXTRAS = {
+    "notebooks": REQUIREMENTS_NOTEBOOKS,
+    "docs": REQUIREMENTS_DOCS,
+    "tests": REQUIREMENTS_TESTS,
+    "deploy": REQUIREMENTS_DEPLOY,
+    "dev": REQUIREMENTS_DEV,
+}
 
 for k, v in REQUIREMENTS_MODELS.items():
     REQUIREMENTS_EXTRAS["model-{}".format(k)] = v
-    REQUIREMENTS_TESTS += v
+    REQUIREMENTS_EXTRAS["tests"] += v
+    REQUIREMENTS_EXTRAS["dev"] += v
 
 # Get the long description from the README file
 with open("README.rst", "r", encoding="utf-8") as f:
@@ -79,35 +109,20 @@ CMDCLASS = versioneer.get_cmdclass()
 CMDCLASS.update({"test": OpenSCMTest})
 
 setup(
-    name="openscm",
+    name=NAME,
     version=versioneer.get_version(),
-    description="A unifying interface for Simple Climate Models",
+    description=SHORT_DESCRIPTION,
     long_description="\n".join(README_LINES),
     long_description_content_type="text/x-rst",
-    author="Robert Gieseke, Jared Lewis, Zeb Nicholls, Sven Willner",
-    author_email="robert.gieseke@pik-potsdam.de, jared.lewis@climate-energy-college.org, zebedee.nicholls@climate-energy-college.org, sven.willner@pik-potsdam.de",
-    url="https://github.com/openclimatedata/openscm",
-    license="GNU Affero General Public License v3.0 or later",
-    classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-    ],
-    keywords=["simple climate model"],
-    packages=["openscm"],
-    install_requires=REQUIREMENTS,
+    keywords=KEYWORDS,
+    author=", ".join([author[0] for author in AUTHORS]),
+    author_email=", ".join([author[1] for author in AUTHORS]),
+    url=URL,
+    project_urls=PROJECT_URLS,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    packages=PACKAGES,
+    install_requires=REQUIREMENTS_INSTALL,
     extras_require=REQUIREMENTS_EXTRAS,
     cmdclass=CMDCLASS,
-    project_urls={
-        "Bug Reports": "https://github.com/openclimatedata/openscm/issues",
-        "Documentation": "https://openscm.readthedocs.io/en/latest",
-        "Source": "https://github.com/openclimatedata/openscm",
-    },
 )
