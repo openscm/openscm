14f6a3b9bb1bd28c79ffcf40f03c3dbd439c170d
Sven Willner <sven.willner@gmail.com>
2019-03-07 11:05:31 +0100

    Remove unused typing comment

--- openscm/__init__.py
+++ openscm/__init__.py
@@ -7,5 +7,5 @@ from .openscm import OpenSCM
 
 from ._version import get_versions
 
-__version__: str = get_versions()["version"]  # type: ignore
+__version__: str = get_versions()["version"]
 del get_versions
