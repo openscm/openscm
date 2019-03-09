add10ef7eab505e8b071471f2b98b529b482d99b
Sven Willner <sven.willner@gmail.com>
2019-03-09 11:18:59 +0100

    Use local attribute instead of global

--- openscm/utils.py
+++ openscm/utils.py
@@ -5,8 +5,6 @@ Utility functions for openscm.
 from typing import Tuple, Union
 import warnings
 
-_ensure_input_is_tuple_calls: int = 0
-
 
 def ensure_input_is_tuple(inp: Union[str, Tuple[str, ...]]) -> Tuple[str, ...]:
     """
@@ -21,10 +19,9 @@ def ensure_input_is_tuple(inp: Union[str, Tuple[str, ...]]) -> Tuple[str, ...]:
     -------
     A tuple with a single string `inp` if `inp` is a string, otherwise return `inp`
     """
-    global _ensure_input_is_tuple_calls  # pylint: disable=global-statement
     if isinstance(inp, str):
-        if not _ensure_input_is_tuple_calls:
-            _ensure_input_is_tuple_calls = 1
+        if not getattr(ensure_input_is_tuple, "calls", 0):
+            setattr(ensure_input_is_tuple, "calls", 1)
             warnings.warn("Converting input {} from string to tuple".format(inp))
         return (inp,)
 
