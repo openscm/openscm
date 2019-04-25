1ec129aead064956e671fa5b0569993acefba88f
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-04-25 18:43:39 +1000

    Remove superceded function

--- openscm/utils.py
+++ openscm/utils.py
@@ -43,22 +43,6 @@ def convert_openscm_time_to_datetime(oscm_in: int) -> datetime.datetime:
     return OPENSCM_REFERENCE_TIME + relativedelta(seconds=oscm_in)
 
 
-def round_to_nearest_year(dtin: datetime.datetime) -> datetime.datetime:
-    """
-    Round a datetime.datetime to Jan 1st 00:00:00 of the nearest year
-
-    thank you https://stackoverflow.com/a/48108115
-    """
-    dt_start_year = dtin.replace(
-        month=1, day=1, minute=0, hour=0, second=0, microsecond=0
-    )
-    dt_half_year = dtin.replace(month=6, day=17)
-    if dtin > dt_half_year:
-        return dt_start_year + relativedelta(years=1)
-    else:
-        return dt_start_year
-
-
 def is_floatlike(f: Any) -> bool:
     """
     Check if input can be cast to a float
