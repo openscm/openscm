736bc3cee339cb830e79b686bb0e5661ef72d854
Jared Lewis <jared@jared.kiwi.nz>
2019-05-06 17:08:38 +1000

    Install pyam in tests

--- setup.py
+++ setup.py
@@ -44,7 +44,14 @@ REQUIREMENTS_NOTEBOOKS = [
     # TODO use pypi version
     "pyam-iamc @ git+https://github.com/IAMconsortium/pyam.git@a6ac0c574c3d5363fb8dba7d8b42a845fd3dbe87",
 ]
-REQUIREMENTS_TESTS = ["codecov", "nbval", "pytest", "pytest-cov"]
+REQUIREMENTS_TESTS = [
+    "codecov",
+    "nbval",
+    "pytest",
+    "pytest-cov",
+    # TODO use pypi version
+    "pyam-iamc @ git+https://github.com/IAMconsortium/pyam.git@a6ac0c574c3d5363fb8dba7d8b42a845fd3dbe87"
+]
 REQUIREMENTS_DOCS = ["sphinx>=1.8", "sphinx_rtd_theme", "sphinx-autodoc-typehints"]
 REQUIREMENTS_DEPLOY = ["setuptools>=38.6.0", "twine>=1.11.0", "wheel>=0.31.0"]
 REQUIREMENTS_DEV = (
