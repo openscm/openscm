febf7c8f042a700a3d9c459384419e0352e5469d
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-07-01 14:01:37 +1000

    Add PH99 model

---
+++ openscm/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+Models maintained within the OpenSCM project.
+
+These are generally small, 'toy' models which do not require a separate package
+to implement (i.e. they have few or no dependencies).
+"""
+from .ph99 import PH99Model  # noqa: F401
--- tests/conftest.py
+++ tests/conftest.py
@@ -595,3 +595,31 @@ def read_magicc7_variables():
 @pytest.fixture(scope="module", params=(read_magicc7_variables()))
 def magicc7_variable(request):
     yield request.param
+
+
+# temporary workaround until this is in Pint itself and can be imported
+def assert_pint_equal(a, b, **kwargs):
+    c = b.to(a.units)
+    try:
+        np.testing.assert_allclose(a.magnitude, c.magnitude, **kwargs)
+
+    except AssertionError as e:
+        original_msg = "{}".format(e)
+        note_line = "Note: values above have been converted to {}".format(a.units)
+        units_lines = "Input units:\n" "x: {}\n" "y: {}".format(a.units, b.units)
+
+        numerical_lines = (
+            "Numerical values with units:\n" "x: {}\n" "y: {}".format(a, b)
+        )
+
+        error_msg = (
+            "{}\n"
+            "\n"
+            "{}\n"
+            "\n"
+            "{}\n"
+            "\n"
+            "{}".format(original_msg, note_line, units_lines, numerical_lines)
+        )
+
+        raise AssertionError(error_msg)
