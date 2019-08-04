06c5d63131bf4d61a2730faee4c1b8272c9b1eea
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-04 10:32:32 +1000

    Add tests of point to average conversion

--- tests/unit/test_timeseries_converter.py
+++ tests/unit/test_timeseries_converter.py
@@ -3,7 +3,8 @@ import re
 import numpy as np
 import pytest
 
-from openscm.core.time import ExtrapolationType, TimeseriesConverter
+from openscm.core.parameters import ParameterType
+from openscm.core.time import ExtrapolationType, InterpolationType, TimeseriesConverter
 from openscm.errors import InsufficientDataError
 
 
@@ -69,3 +70,50 @@ def test_no_overlap(combo, miss_type):
         combo.interpolation_type,
         ExtrapolationType.LINEAR,
     )
+
+
+def test_point_to_average_conversion():
+    point_times = np.array([  0, 1, 2, 5, 10])
+    point_values = np.array([-1, 2, 3, 6, 5.5])
+
+    average_times = np.array([0, 2, 3, 10])
+    average_values = np.array([1, 2.5, 6.5])
+
+    converter_point_to_average = TimeseriesConverter(
+        point_times,
+        average_times,
+        ParameterType.POINT_TIMESERIES,
+        InterpolationType.LINEAR,
+        ExtrapolationType.NONE,
+        timeseries_type_target=ParameterType.AVERAGE_TIMESERIES,
+    )
+    converter_average_to_point_circular = TimeseriesConverter(
+        average_times,
+        point_times,
+        ParameterType.AVERAGE_TIMESERIES,
+        InterpolationType.LINEAR,
+        ExtrapolationType.NONE,
+        timeseries_type_target=ParameterType.POINT_TIMESERIES,
+    )
+
+    point_to_average_result = converter_point_to_average.convert_from(point_values)
+    point_to_average_expected = np.array([1.5, 3.5, 6.25])
+    np.testing.assert_array_equal(point_to_average_result, point_to_average_expected)
+
+    average_to_point_result_circular = converter_average_to_point_circular.convert_from(
+        point_to_average_result
+    )
+    np.testing.assert_array_equal(average_to_point_result_circular, point_values)
+
+    converter_average_to_point = TimeseriesConverter(
+        average_times,
+        np.arange(0, 11),
+        ParameterType.AVERAGE_TIMESERIES,
+        InterpolationType.LINEAR,
+        ExtrapolationType.NONE,
+        timeseries_type_target=ParameterType.POINT_TIMESERIES,
+    )
+    average_to_point_result = converter_average_to_point.convert_from(
+        average_values
+    )
+    np.testing.assert_array_equal(average_to_point_result, np.arange(0, 11))
